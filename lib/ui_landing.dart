2aa71d5c692cd291049a9e4eccd79444d18cee31
ilshookim <ilshookim@gmail.com>
2021-04-04 19:24:33 +0900

    ADD images ADD landing, components pages

--- lib/ui_landing.dart
+++ lib/ui_landing.dart
@@ -9,55 +9,51 @@ class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 32),
-              child: Column(
-                children: [
-                  MenuBar(),
-                  ListItem(
-                      imageUrl:
-                          "assets/images/paper_flower_overhead_bw_w1080.jpg",
-                      title: listItemTitleText,
-                      description: listItemPreviewText),
-                  divider,
-                  ListItem(
-                      imageUrl:
-                          "assets/images/iphone_cactus_tea_overhead_bw_w1080.jpg",
-                      title: listItemTitleText,
-                      description: listItemPreviewText),
-                  divider,
-                  ListItem(
-                      imageUrl:
-                          "assets/images/typewriter_overhead_bw_w1080.jpg",
-                      title: listItemTitleText,
-                      description: listItemPreviewText),
-                  divider,
-                  ListItem(
-                      imageUrl:
-                          "assets/images/coffee_paperclips_pencil_angled_bw_w1080.jpg",
-                      title: listItemTitleText,
-                      description: listItemPreviewText),
-                  divider,
-                  ListItem(
-                      imageUrl:
-                          "assets/images/joy_note_coffee_eyeglasses_overhead_bw_w1080.jpg",
-                      title: listItemTitleText,
-                      description: listItemPreviewText),
-                  divider,
-                  Container(
-                    padding: EdgeInsets.symmetric(vertical: 80),
-                    child: ListNavigation(),
-                  ),
-                  divider,
-                  Footer(),
-                ],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 32),
+          child: Column(
+            children: [
+              MenuBar(),
+              ListItem(
+                  imageUrl:
+                      "assets/images/paper_flower_overhead_bw_w1080.jpg",
+                  title: listItemTitleText,
+                  description: listItemPreviewText),
+              divider,
+              ListItem(
+                  imageUrl:
+                      "assets/images/iphone_cactus_tea_overhead_bw_w1080.jpg",
+                  title: listItemTitleText,
+                  description: listItemPreviewText),
+              divider,
+              ListItem(
+                  imageUrl:
+                      "assets/images/typewriter_overhead_bw_w1080.jpg",
+                  title: listItemTitleText,
+                  description: listItemPreviewText),
+              divider,
+              ListItem(
+                  imageUrl:
+                      "assets/images/coffee_paperclips_pencil_angled_bw_w1080.jpg",
+                  title: listItemTitleText,
+                  description: listItemPreviewText),
+              divider,
+              ListItem(
+                  imageUrl:
+                      "assets/images/joy_note_coffee_eyeglasses_overhead_bw_w1080.jpg",
+                  title: listItemTitleText,
+                  description: listItemPreviewText),
+              divider,
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 80),
+                child: ListNavigation(),
               ),
-            ),
+              divider,
+              Footer(),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
